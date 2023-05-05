c3bc84516397147bf54988294b03451a26731ace
ErrorCommander <error.commander.gd@gmail.com>
2023-05-05 05:32:10 +0300

    apply game settings for gifr place

--- Assets/GameResource/Scripts/Interactive Elements/GiftPlace.cs
+++ Assets/GameResource/Scripts/Interactive Elements/GiftPlace.cs
@@ -1,12 +1,29 @@
 using Gameplay.ItemInfo;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay.Interactive
 {
     public class GiftPlace : InteractiveItemPlace<BoxInfo>
     {
+        [SerializeField, Range(0,4)] private int _indexPlace;
         [SerializeField] private Gift _gift;
-        
+        [SerializeField] private Image _imageDesk;
+        [SerializeField] private Sprite _activeSprite;
+        [SerializeField] private Sprite _disableSprite;
+
+        private void Start()
+        {
+            SetActivePlace(_indexPlace < GameMode.CurrentSettings.GiftPlaceCount);
+        }
+
+        private void SetActivePlace(bool state)
+        {
+            _gift.gameObject.SetActive(state);
+            _imageDesk.sprite = state ? _activeSprite : _disableSprite;
+            _imageDesk.raycastTarget = state;
+        }
+
         public override bool TryAddItem(BoxInfo item)
         {
             return _gift.AddElement(item);
