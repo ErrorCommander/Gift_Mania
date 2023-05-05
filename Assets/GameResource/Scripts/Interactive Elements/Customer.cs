5309a57578560c474dd18748371a12b8c3f8dfab
ErrorCommander <error.commander.gd@gmail.com>
2023-05-05 05:31:33 +0300

    customer check gift

--- Assets/GameResource/Scripts/Interactive Elements/Customer.cs
+++ Assets/GameResource/Scripts/Interactive Elements/Customer.cs
@@ -1,24 +1,62 @@
+using Gameplay.Infrastructures;
 using Gameplay.ItemInfo;
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay.Interactive
 {
     public class Customer : InteractiveItemPlace<GiftInfo>
     {
         public event Action<float, float> OnChangeWaitTime = delegate {  };
+        public event Action OnFailOrder = delegate {  };
+        public event Action<int> OnSuccessOrder = delegate {  };
 
+        [SerializeField] private GiftSpriteContainer _spriteContainer;
+        [SerializeField] private Image _customerImage;
+        [SerializeField] private Image _giftImage;
+        [SerializeField] private int _reward = 20;
+
+        private Sprite _happy;
+        private Sprite _sad;
+        private Coroutine _wait;
+        private GiftInfo _gift;
 
         public override bool TryAddItem(GiftInfo item)
         {
-            Debug.Log($"{name} AddItem " + item);
-            return true;
+            if (item.Equals(_gift))
+            {
+                Debug.Log($"{name} AddItem " + item);
+                _gift = null;
+                OnSuccessOrder.Invoke(_reward);
+                return true;
+            }
+
+            Debug.Log($"{name} dont get item " + item);
+            return false;
         }
 
-        private void OnEnable()
+        public void Visit(float waitTime, Sprite happy, Sprite sad)
         {
-            StartCoroutine(Wait(30));
+            gameObject.SetActive(true);
+
+            _happy = happy;
+            _sad = sad;
+            _customerImage.sprite = _happy;
+            
+            StopWait();
+            if (waitTime > 0) 
+                _wait = StartCoroutine(Wait(waitTime));
+
+            _gift = new GiftInfo(GameMode.GetRandomGiftCode());
+            _giftImage.sprite = _spriteContainer.GetSprite(_gift);
+        }
+
+        private void StopWait()
+        {
+            if(_wait != null)
+                StopCoroutine(_wait);
         }
 
         private IEnumerator Wait(float second)
@@ -30,8 +68,10 @@ namespace Gameplay.Interactive
                 OnChangeWaitTime.Invoke(timer,second);
                 yield return null;
             }
-            
+
+            _wait = null;
             OnChangeWaitTime.Invoke(0, second);
+            OnFailOrder.Invoke();
         }
     }
 }
